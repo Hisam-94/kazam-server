dcd596ac06c5bc6da6d304eabe51faa27cb62806
Hisamuddin Shaikh <hisamuddin94@gmail.com>
2025-02-08 19:01:19 +0530

    Completed

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,21 +1,23 @@
 import { Request, Response } from 'express';
-const User = require('../models/user.model');
+import userModel from '../models/user.model';
 const { registerSchema, loginSchema } = require('../middlewares/validate');
 const jwt = require('jsonwebtoken');
 
 export const register = async (req: Request, res: Response) => {
+  console.log("req",req)
   try {
     const { error } = registerSchema.validate(req.body);
     if (error) return res.status(400).json({ message: error.message });
 
     const { name, email, password } = req.body;
+    console.log("name, email, password",name, email, password)
     
-    const existingUser = await User.findOne({ email });
+    const existingUser = await userModel.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    const user = await User.create({ name, email, password });
+    const user = await userModel.create({ name, email, password });
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
       expiresIn: '1h'
     });
@@ -25,6 +27,7 @@ export const register = async (req: Request, res: Response) => {
       user: { id: user._id, name: user.name, email: user.email } 
     });
   } catch (error) {
+    console.log("error",error)
     res.status(500).json({ message: 'Server error' });
   }
 };
@@ -35,7 +38,7 @@ export const login = async (req: Request, res: Response) => {
     if (error) return res.status(400).json({ message: error.message });
 
     const { email, password } = req.body;
-    const user = await User.findOne({ email }).select('+password');
+    const user = await userModel.findOne({ email }).select('+password');
     
     if (!user || !(await user.comparePassword(password))) {
       return res.status(401).json({ message: 'Invalid credentials' });
